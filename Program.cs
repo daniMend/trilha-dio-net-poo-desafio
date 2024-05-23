41efefade7330e8d2ffb4b7da2867955dbe9c1d6
daniMend <dnmendonca@gmail.com>
2024-05-23 18:35:27 -0300

    Desafio resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia= new Nokia("99999-9999", "Nokia1", "IMEI1111222", 256);
+
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+
+Iphone iphone= new Iphone("98888-8888", "Iphone15", "IMEI33334444", 512);
+
+iphone.InstalarAplicativo("Telegram");
+iphone.Ligar();
+iphone.ReceberLigacao();
